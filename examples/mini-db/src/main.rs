4bb8a9dfe6b61b13dc08bff093aecffd99e2c50a
Johannes Sluis <joesluis51@gmail.com>
2026-03-28 11:34:58 -0700

    init mini db

---
+++ examples/mini-db/src/main.rs
@@ -0,0 +1,65 @@
+use std::error::Error;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io;
+use std::io::{BufWriter, Write};
+
+#[derive(PartialEq)]
+enum OperationType {
+    Set,
+    Get,
+}
+
+struct Operation {
+    operation_type: OperationType,
+    key: String,
+    value: Option<String>,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("db.log")?;
+
+    let mut writer = BufWriter::new(&file);
+
+    loop {
+        println!("Enter DB operation (e.g. db set 1 hi) ");
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read input");
+
+        let curr_op = parse_line(&input)?;
+
+        if curr_op.operation_type == OperationType::Get {
+        } else {
+            let record = format!("{} {}\n", curr_op.key, curr_op.value.unwrap());
+            writer.write_all(record.as_bytes())?;
+
+            writer.flush()?;
+        }
+    }
+}
+
+fn parse_line(input: &str) -> Result<Operation, Box<dyn Error>> {
+    let vec: Vec<&str> = input.split_whitespace().collect();
+
+    match vec.as_slice() {
+        ["db", "get", key] => Ok(Operation {
+            operation_type: OperationType::Get,
+            key: key.to_string(),
+            value: None,
+        }),
+        ["db", "set", key, value] => Ok(Operation {
+            operation_type: OperationType::Set,
+            key: key.to_string(),
+            value: Some(value.to_string()),
+        }),
+        _ => Err("invalid input".into()),
+    }
+}
+
+// fn search_file(operation: OperationType, )
