4a2de1aa9c7df62804c29e5ffae054b5ffe24f1e
Johannes Sluis <joesluis51@gmail.com>
2026-05-08 00:24:02 -0700

    Add table for system info

--- examples/process-monitor/src/main.rs
+++ examples/process-monitor/src/main.rs
@@ -1,9 +1,29 @@
+use comfy_table::Table;
 use sysinfo::{Components, Disks, Networks, System};
 
 fn main() {
-    let mut sys = System::new_all();
+    let mut table = Table::new();
 
+    let mut sys = System::new_all();
     sys.refresh_all();
 
-    println!("total mem: {} bytes", sys.total_memory());
+    table
+        .set_header(vec![
+            "System name",
+            "Kernel version",
+            "OS version",
+            "host name",
+        ])
+        .add_row(vec![
+            &System::name().unwrap(),
+            &System::kernel_version().unwrap(),
+            &System::os_version().unwrap(),
+            &System::host_name().unwrap(),
+        ]);
+
+    for (pid, process) in sys.processes() {
+        println!("[{pid}] {:?} {:?}", process.name(), process.disk_usage());
+    }
+
+    println!("{table}");
 }
