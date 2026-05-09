6aabb138a4c1f90c9ee527912087d3561b7d0e9b
Johannes Sluis <joesluis51@gmail.com>
2026-05-09 00:56:13 -0700

    show processes in comfy table

--- examples/process-monitor/src/main.rs
+++ examples/process-monitor/src/main.rs
@@ -15,27 +15,29 @@ fn main() {
             "host name",
         ])
         .add_row(vec![
-            &System::name().unwrap(),
-            &System::kernel_version().unwrap(),
-            &System::os_version().unwrap(),
-            &System::host_name().unwrap(),
+            System::name().unwrap_or_else(|| "unknown".to_string()),
+            System::kernel_version().unwrap_or_else(|| "unknown".to_string()),
+            System::os_version().unwrap_or_else(|| "unknown".to_string()),
+            System::host_name().unwrap_or_else(|| "unknown".to_string()),
         ]);
+    println!("{table}");
 
     let mut processes: Vec<_> = sys.processes().iter().collect();
+    let mut process_table = Table::new();
+    process_table.set_header(vec!["PID", "Name", "Memory MiB", "CPU %", "Status"]);
     processes.sort_by_key(|(_, process)| process.memory());
     processes.reverse();
 
     for (pid, process) in processes.iter().take(10) {
         let mem_usage = process.memory() as f64 / 1024.0 / 1024.0;
-        println!(
-            "{:?} {:?} {:?}mb {:?} {:?}",
-            pid,
-            process.name(),
-            mem_usage,
-            process.cpu_usage(),
-            process.status()
-        );
+        process_table.add_row(vec![
+            pid.to_string(),
+            process.name().to_string_lossy().to_string(),
+            mem_usage.to_string(),
+            process.cpu_usage().to_string(),
+            process.status().to_string(),
+        ]);
     }
-
-    println!("{table}");
+    println!("top 10 processes by memory");
+    println!("{process_table}");
 }
